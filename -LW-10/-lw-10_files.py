f2c0fce80bb243961c3c7599e7ffa859dd091719
Serhii <sbatracenko@gmail.com>
2024-05-06 14:21:36 +0300

    LW10_SecondStudent

--- -LW-10/-lw-10_files.py
+++ -LW-10/-lw-10_files.py
@@ -18,20 +18,14 @@ except Exception as e:
 #Batrachenko Serhii
 # Ваше прізвище
 new_surname = "Батраченко"
-
-# Питання з програмування
-question = "На що вказує параметр w при відкритті файлу?"
 answer = "Параметр 'w' при відкритті файлу вказує на те, що файл відкривається для запису. Якщо файл вже існує, вміст файлу буде замінений новим. Якщо файл не існує, він буде створений."
 
 # Запитання для наступного студента
 next_question = "Які існують режими відкриття файлу у Python?"
 
-file_name = "questions.txt"
-
 try:
     with open(file_name, 'a', encoding='utf-8') as file:  # Відкриваємо файл у режимі додавання
         file.write("Прізвище: {}\n".format(new_surname))
-        file.write("Питання: {}\n".format(question))
         file.write("Відповідь: {}\n".format(answer))
         file.write("\n")  # Додаємо порожній рядок між записами
         file.write("Наступне питання: {}\n".format(next_question))
